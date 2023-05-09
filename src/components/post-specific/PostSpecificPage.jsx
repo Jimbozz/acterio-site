b17f401ce083602966c5207b2241f9e7e62d6f79
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2023-05-09 17:57:12 +0200

    added layout to page

--- src/components/post-specific/PostSpecificPage.jsx
+++ src/components/post-specific/PostSpecificPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Layout from "../../layout/Layout";
+import PostSpecific from "./PostSpecific";
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { IoArrowBack } from "react-icons/io5";
 
-function PostSpecificPage() {
-  return <div>PostSpecificPage</div>;
+export default function PostSpecificPage() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1, { replace: true });
+  };
+  return (
+    <Layout>
+      <Button variant="link" onClick={handleClick}>
+        <IoArrowBack /> Back
+      </Button>
+      <PostSpecific />
+    </Layout>
+  );
 }
-
-export default PostSpecificPage;
