cd2fdc9826fb0af7502bb4e070fcaec9c0d405c7
Jimbozz <79139294+Jimbozz@users.noreply.github.com>
2023-05-09 17:55:33 +0200

    added links

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,27 +1,24 @@
-import React from "react";
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className="sticky-top" expand="lg" bg="light">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <NavLink className="navbar-brand" to="/">
+          ACTERIO SITE
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/posts">
+              Blog
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
